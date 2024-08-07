d0dbde7d4fb4a6a821ba15a77bec99545d77b9f8
KellyDanielO <kizdver@gmail.com>
2024-08-07 16:52:43 +0100

    changed api link

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConstants{
-  static String baseUrl = 'https://skillrader-backend.onrender.com/api';
+  static String baseUrl = 'https://api.skillradarapp.com/api';
   static String geminiAPIKey = dotenv.env['GEMINI_API_KEY']!;
 }
